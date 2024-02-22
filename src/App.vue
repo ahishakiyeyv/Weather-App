b3a62fe8dc964a1030eabdde4a8f36a8f1563e78
ahishakiyeyv <yvanahishakiye6@gmail.com>
2024-02-23 00:06:10 +0100

    design added

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,26 @@
 <div class="app">
   <div class="header container h-100 p-5">
     <h1 class="mb-5">Weather App</h1>
-    <div class="d-flex.justify-content-center h-100">
+    <div class="d-flex justify-content-center h-100">
       <div class="searchbar w-50 mx-2">
         <input type="text" class="input form-control" placeholder="Enter the city name...">
-        <button class="btn-search btn btn-primary">Search</button>
+        
       </div>
+      <button class="btn-search btn btn-primary">Search</button>
     </div>
   </div>
 </div>
 </template>
 
 <style>
-
+body{
+  background:#121212 !important;
+}
+.header{
+  background:#212730;
+  border-radius:20px;
+  color:#f9f9f9;
+  text-align:center;
+  margin-top:5rem;
+}
 </style>
