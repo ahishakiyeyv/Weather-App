773c47fd3e087548bbb2b998ba8469696137aa66
ahishakiyeyv <yvanahishakiye6@gmail.com>
2024-02-23 00:00:40 +0100

    page content added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div class="app">
+  <div class="header container h-100 p-5">
+    <h1 class="mb-5">Weather App</h1>
+    <div class="d-flex.justify-content-center h-100">
+      <div class="searchbar w-50 mx-2">
+        <input type="text" class="input form-control" placeholder="Enter the city name...">
+        <button class="btn-search btn btn-primary">Search</button>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
